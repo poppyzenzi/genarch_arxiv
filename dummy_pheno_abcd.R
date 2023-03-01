eea634d371c950fb21f198b9be861002064d7786
poppyzenzi <pzgrimes1@gmail.com>
2023-03-01 17:27:14 +0000

    Rscript to generate dummy pheno

--- dummy_pheno_abcd.R
+++ dummy_pheno_abcd.R
@@ -1,3 +1,7 @@
+library(magrittr)
+library(car)
+library(dplyr)
+
 # Create binary pheno file where 1 indicates precence of disorder and 0 none
 # First two columns should be FID (family id) and IID (sampel id)
 # each pheno column generates a separate PRS output file in PRSice-2
@@ -12,10 +16,40 @@ fam <- read.table("ABCD3.0_imputed_whiteonly_MAF0.01_unrelated.fam")
 
 #extracts FID and IID as the NDAR IDs from the unrelated sample
 # FID contains no multiple values for an unrelated sample. So if unrelated, FID and IID will be different
-pheno <- data.frame("FID" = fam[,1], "IID" = fam[,1], "no_pheno" = 0)
+pheno <- data.frame("IID" = fam[,2], "no_pheno" = 0)
 
 # Add random 0 and 1s to no_pheno
 pheno$no_pheno <- sample(c(0,1), replace = T, size = nrow(pheno))
 
-# save table in your scratch dir - use this with --pheno-file command in PRSice 
-write.table(pheno, "no_pheno.txt", row.names = F, quote = F)
+
+df <- readRDS('/exports/eddie/scratch/s2421111/abcd/abcd_cbcls01.rds')
+
+df <- df %>% subset(select = c('src_subject_id', 'eventname',
+                               'cbcl_scr_dsm5_depress_r', 'interview_age', 'sex')) %>%
+  set_colnames(c("IID","time","dep", "age", "sex"))
+
+time_recode <- dplyr::recode(df$time,
+                             `baseline_year_1_arm_1`="0",
+                             `1_year_follow_up_y_arm_1`="1",
+                             `2_year_follow_up_y_arm_1`="2",
+                             `3_year_follow_up_y_arm_1`="3")
+
+
+df$time <- as.numeric(time_recode)
+df$age <- as.numeric(df$age)/12 #convert age to years
+df$dep <- as.numeric(df$dep)
+
+sex_recode <- dplyr::recode(df$sex, #recode sex to binary
+                            `M`="0",
+                            `F`="1"
+)
+
+df$sex <- as.numeric(sex_recode)
+df <- subset(df, time == 1)
+
+
+pheno <- merge(pheno, df, by = c("IID"))
+
+pheno
+# save table in your scratch dir - use this with --pheno-file command in PRSice
+write.table(pheno, "pheno_file.txt", row.names = F, quote = F)
