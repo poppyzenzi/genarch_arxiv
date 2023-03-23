5cd1a07132d226e439b3aee0719648cd2a039e87
poppyzenzi <pzgrimes1@gmail.com>
2023-03-23 11:18:31 +0000

    Neff anx calc finalised

--- GenomicSEM/Neff_calc_anx19.R
+++ GenomicSEM/Neff_calc_anx19.R
@@ -1,26 +1,29 @@
+# R script to calculate Neffective size in Purves 2019 anxiety sumstats (maybe also neu)
+# see here: https://github.com/GenomicSEM/GenomicSEM/wiki/2.1-Calculating-Sum-of-Effective-Sample-Size-and-Preparing-GWAS-Summary-Statistics
+
 #load in required packages
 require(data.table)
 require(dplyr)
 
 # set wd
-setwd("/exports/igmm/eddie/GenScotDepression/users/poppy/PRS/GWAS")
+setwd("/exports/eddie/scratch/s2421111/gwas")
 
-#read in Anxiety summary stats from Purves et al. 2019
-ANX<-gzfile("TotAnx_effect_sumstats_MAF0.01_INFO0.8_nodup_noambig.gz",data.table=FALSE)
-# or try
-ANX<-read.table("TotAnx_effect_sumstats_MAF0.01_INFO0.8_nodup_noambig.gz", header = TRUE)
+# read in Anxiety summary stats from Purves et al. 2019
+ANX <- fread("META_UKBB_iPSYCH_ANGST_wNcol.sumstats", data.table = FALSE)
 
+# read in 1000 genomes reference file used to get approximation of SNP MAF as MAF not present in the anxiety sumstats
+setwd("/exports/igmm/eddie/GenScotDepression/users/poppy/gsem/munging")
+ref <- fread("reference.1000G.maf.0.005.txt", data.table = FALSE)
 
-#read in 1000 genomes reference file used to get approximation of SNP MAF
-#as MAF not present in the anxiety summary statistics file
-ref<-fread("reference.1000G.maf.0.005.txt",data.table=FALSE)
+# subset reference file to just SNP and MAF
+ref <- ref[, c("SNP", "MAF")]
 
-#subset reference file to just SNP and MAF
-attach(ref)
-ref<-data.frame(SNP,MAF)
+# convert SNP column in ANX to a factor variable with the same levels as ref$SNP
+ANX$SNP <- factor(ANX$SNP, levels = ref$SNP)
 
-#merge Anxiety and reference file
-ANX<-inner_join(ANX,ref,by="SNP",all=F)
+# merge ANX and reference file
+ANX <- inner_join(ANX, ref, by = "SNP", all = FALSE)
+
 
 #calculate effective sample size implied by GWAS summary statistics
 ANX$Neff<-4/((2*ANX$MAF*(1-ANX$MAF))*ANX$StdErr^2)
@@ -44,4 +47,5 @@ ANX$MAF<-NULL
 ANX$TotalSampleSize<-NULL
 
 #output the summary stats with the SNP-specific effective sample size column
+setwd("/exports/igmm/eddie/GenScotDepression/users/poppy/PRS/GWAS")
 write.table(ANX, file = "ANX_withNeff.txt", sep = "\t", quote=FALSE,row.names=FALSE,col.names=TRUE)
