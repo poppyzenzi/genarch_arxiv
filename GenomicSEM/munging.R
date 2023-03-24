747ed7a562eb2046655e1a1b1932c6b5ec6e5c69
poppyzenzi <pzgrimes1@gmail.com>
2023-03-24 12:11:39 +0000

    munging script complete

--- GenomicSEM/munging.R
+++ GenomicSEM/munging.R
@@ -1,17 +1,3 @@
-#!/bin/bash
-#$ -N munging_7_sumstats
-#$ -l h_rt=1:00:00
-#$ -l h_vmem=64G
-#$ -pe sharedmem 1
-#$ -e /exports/igmm/eddie/GenScotDepression/users/poppy/gsem/job_logs
-#$ -o /exports/igmm/eddie/GenScotDepression/users/poppy/gsem/job_logs
-#$ -M p.grimes@ed.ac.uk
-#$ -m baes
-
-# load modules
-. /etc/profile.d/modules.sh
-module unload igmm/apps/R/3.5.1
-module load igmm/apps/R/4.1.0
 
 #this is an R script (on EDDIE) to munge files for step of GSEM
 
