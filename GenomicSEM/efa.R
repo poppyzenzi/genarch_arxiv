0878431975b304a781202fac2cd6a0065db3361a
Poppy Grimes <pzgrimes1@gmail.com>
2023-04-12 11:56:26 +0200

    exploratory factor analysis

--- GenomicSEM/efa.R
+++ GenomicSEM/efa.R
@@ -1,12 +1,11 @@
 require(GenomicSEM)
 library(GenomicSEM)
-
+library(readr)
 # munged sum stats in "/exports/igmm/eddie/GenScotDepression/users/poppy/PRS/GWAS/"
 
 
 #run multivariable LDSC to create the S and V matrices
-psycho <- "/exports/igmm/eddie/GenScotDepression/users/poppy/gsem/ldsc/LDSCoutput.RData"
-load(psycho)
+psycho <- readRDS("/exports/igmm/eddie/GenScotDepression/users/poppy/gsem/ldsc/LDSCoutput.rds")
 
 #smooth the S matrix for EFA using the nearPD function in the Matrix package.
 require(Matrix)
