4b1c702503fea63a9601796bc7af2805c4ff5d26
Poppy Grimes <pzgrimes1@gmail.com>
2023-04-21 15:56:15 +0200

    parcel factors sum stats calc

--- GenomicSEM/parcel_factors/parcels_sumstats.R
+++ GenomicSEM/parcel_factors/parcels_sumstats.R
@@ -1,9 +1,9 @@
-# step 4: prep sum stats for GWAS
+# step 4: prep sum stats for GWAS for 3 parcels
 require(GenomicSEM)
 library(GenomicSEM)
 
+# ======================= mood ============================
 setwd("/exports/igmm/eddie/GenScotDepression/users/poppy/PRS/GWAS/")
-
 files<-c("mdd3_ss_MAF.txt", "luc_neur_ss.txt", "ANX_withNeff.txt")
 ref= "/exports/igmm/eddie/GenScotDepression/users/poppy/gsem/munging/reference.1000G.maf.0.005.txt"
 trait.names<-c('MDD', 'NEU', 'ANX')
@@ -15,23 +15,42 @@ MOOD_sumstats <-sumstats(files=files,ref=ref,trait.names=trait.names,
                             betas=NULL,info.filter=0.8,maf.filter=maf.0.01,
                             keep.indel=FALSE,parallel=TRUE,cores=NULL)
 
-setwd("/exports/igmm/eddie/GenScotDepression/users/poppy/gsem/")
+setwd("/exports/igmm/eddie/GenScotDepression/users/poppy/gsem/parcel_factors/sumstats/")
+saveRDS(MOOD_sumstats, file="MOOD_sumstats.rds")
 
-# =====================================================================================================================
+# ======================= neurodev ============================
 
-LDSCoutput = "/exports/igmm/eddie/GenScotDepression/users/poppy/gsem/ldsc/parcel_factors/LDSCoutput_mood.RData"
+setwd("/exports/igmm/eddie/GenScotDepression/users/poppy/PRS/GWAS/")
+files<-c("daner_adhd_meta_MAF0.01_INFO0.8_nodup_noambig.gz", "iPSYCH-PGC_ASD_Nov2017_INFO0.8_nodup_noambig.gz")
+ref= "/exports/igmm/eddie/GenScotDepression/users/poppy/gsem/munging/reference.1000G.maf.0.005.txt"
+trait.names<-c('ADHD', 'ASD')
+se.logit=c(T,T)
+N=c(NA,43777.81914)
 
-#run the multivariate GWAS using parallel processing
-MOOD_parcel <- commonfactorGWAS(covstruc = LDSCoutput, SNPs = MOOD_sumstats, estimation = "DWLS",
-                                    cores = NULL, toler = FALSE, SNPSE = FALSE, parallel = TRUE,
-                                    Output = NULL,GC="standard",MPI=FALSE)
+NEURODEV_sumstats <-sumstats(files=files,ref=ref,trait.names=trait.names,
+                            se.logit=se.logit,OLS=NULL,linprob=NULL,N=N,
+                            betas=NULL,info.filter=0.8,maf.filter=maf.0.01,
+                            keep.indel=FALSE,parallel=TRUE,cores=NULL)
 
+setwd("/exports/igmm/eddie/GenScotDepression/users/poppy/gsem/parcel_factors/sumstats/")
+saveRDS(NEURODEV_sumstats, file="NEURODEV_sumstats.rds")
 
-# note that the code written above specifies all arguments for completeness, but as many of these arguments
-# are set to the package default it could also be written as below and produce the same result:
-MOOD_parcel  <- commonfactorGWAS(covstruc = LDSCoutput, SNPs = MOOD_sumstats)
+# ======================= psychotic ============================
 
-save(MOOD_parcel, file="MOOD_GWAS.txt")
+setwd("/exports/igmm/eddie/GenScotDepression/users/poppy/PRS/GWAS/")
+files<-c("pgc_bip_qcd.txt","pgc_scz_qcd.txt")
+ref= "/exports/igmm/eddie/GenScotDepression/users/poppy/gsem/munging/reference.1000G.maf.0.005.txt"
+trait.names<-c('BIP', 'SCZ')
+se.logit=c(T,T)
+N=c(NA,NA)
+
+PSYCHOTIC_sumstats <-sumstats(files=files,ref=ref,trait.names=trait.names,
+                            se.logit=se.logit,OLS=NULL,linprob=NULL,N=N,
+                            betas=NULL,info.filter=0.8,maf.filter=maf.0.01,
+                            keep.indel=FALSE,parallel=TRUE,cores=NULL)
+
+setwd("/exports/igmm/eddie/GenScotDepression/users/poppy/gsem/parcel_factors/sumstats/")
+saveRDS(PSYCHOTIC_sumstats, file="PSYCHOTIC_sumstats.rds")
 
 
 
