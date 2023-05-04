c887214a3eaf270057530c939f70ab1df4ea9706
Poppy Grimes <pzgrimes1@gmail.com>
2023-05-04 21:23:27 +0100

    fixed GMM x PRS association

--- abcd_prs_predict.py
+++ abcd_prs_predict.py
@@ -1,132 +1,50 @@
 import numpy as np
-import os
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 import pyreadr
-import sklearn
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-import scipy.stats as stats
 import statsmodels.api as sm
-from pathlib import Path
-import random
-import glob
 from functools import reduce
 import os.path
-from os import path
 from sklearn import preprocessing
 
+# ================================ APPEND CLASS ENUMERATION =================================
 
-# ============= WIP script for genetic prediction models in ABCD =================
-
-# first input class data from mplus output > merge with original NDAR IDs
-# read in, select, clean and normalise all variables [0,1]
-# build design matrix
-# impute missing data
-# split test and train > run ML models
-# evaluate feature importance
-
-# class data
+# read in class data from mplus output
 bpm_4k = pd.read_table('/Users/poppygrimes/Library/CloudStorage/OneDrive-UniversityofEdinburgh/Edinburgh/gmm/gmm_abcd/'
-                      'mplus_data/4k_probs_bpm.txt', delim_whitespace=True, header=None)
+                      'mplus_data/4k_gmm_bpm.txt', delim_whitespace=True, header=None)
 bpm_4k.columns = ['y0.5','y1','y1.5','y2','y2.5','y3','v1','v2','v3','v4','v5','v6','v7','v8','v9','v10','class','id']
 bpm_4k = bpm_4k[['id','class']] # subset
 bpm_4k.replace('*', np.nan) # fill missing
 
 # class data has unique id's only > merge with anth to align NDAR id's
-os.chdir('/Users/poppygrimes/Library/CloudStorage/OneDrive-UniversityofEdinburgh/Edinburgh/gmm/gmm_abcd')
+anth = pd.read_csv("/Volumes/igmm/GenScotDepression/users/poppy/abcd/abcd_anth.csv") # 11,733 nunique
 
-# if running script on eddie
-# from abcd_PREP.py import get_dataframes
-# df1, df2 = get_dataframes()
+data = pd.merge(bpm_4k, anth, on='id') # append class enumeration
+print('class data merged')
 
-# if local
-# anth cols: [id time age sex og_id]
-anth = pd.read_csv("/Volumes/igmm/GenScotDepression/users/poppy/abcd/abcd_anth.csv")
-# append class enumeration
-bpm_4k = pd.merge(bpm_4k, anth, on='id')
+data['id'].nunique() # check unique id's
 
-print('class data appended')
+# ================================ VARIABLES =====================================
 
-# ================================VARIABLES=====================================
-# make df with all vars, clean all vars (X), append class col (Y)
-
-data = bpm_4k
 data = data.rename(columns={'og_id': 'src_subject_id', 'sex': 'SEX', 'time': 'eventname'})
 print(data) # this should include mapped numeric and og id's and class enumeration
 
-# =================== add bpm score ===================
-
-# change to var storage area
-os.chdir('/Volumes/igmm/GenScotDepression/data/abcd/release4.0/iii.data/')
-# all files containing VOIs
-rds = ['abcd_yssbpm01.rds']
-
-datasets = [data] # list of datasets with the base data in
-count = 1 # 1 df already in list
-
-# get all dataframes from rds list and append using os.walk
-for root, dirs, files in os.walk(".", topdown=False):
-   for file in files:
-      if file in rds:
-          df = pyreadr.read_r(os.path.join(root, file))
-          df = df[None]  # changes from dict
-          datasets.append(df)
-          count = count + 1
-
-# to prevent duplicate cols
-for df in datasets:
-    df.drop(['interview_date','interview_age','sex'], axis=1, inplace=True, errors='ignore')
-    df['eventname'] = df['eventname'].replace(['baseline_year_1_arm_1', '6_month_follow_up_arm_1', '1_year_follow_up_y_arm_1',
-                                               '18_month_follow_up_arm_1', '2_year_follow_up_y_arm_1', '30_month_follow_up_arm_1',
-                                               '3_year_follow_up_y_arm_1'], [0, 0.5, 1, 1.5, 2, 2.5, 3],)
-    df['eventname']=df['eventname'].astype(float) # all floats for merging
-    df['src_subject_id']=df['src_subject_id'].astype(object) # all objects for merging
-
-print('done - datasets is a list of dataframes')
-
-# merging all datasets on id and time point
-all = reduce(lambda left,right: pd.merge(left,right,on=['src_subject_id','eventname'], how='outer'), datasets)
-
-# filling NaNs in data cols
-# Create a dictionary for each
-id_class_dict = all.dropna(subset=['class']).set_index('src_subject_id')['class'].to_dict()
-id_sex_dict = all.dropna(subset=['SEX']).set_index('src_subject_id')['SEX'].to_dict()
-id_event_age_dict = all.dropna(subset=['age']).set_index(['src_subject_id', 'eventname'])['age'].to_dict() # age changes with time
-
-# Use the dictionary to fill NaN values in specified column
-all['class'] = all['class'].fillna(all['src_subject_id'].map(id_class_dict))
-all['SEX'] = all['SEX'].fillna(all['src_subject_id'].map(id_sex_dict))
-all['age'] = all.apply(lambda row: id_event_age_dict.get((row['src_subject_id'], row['eventname']), row['age']), axis=1)
-
-print('all is a single df')
-
 # remove the numeric id col used in mplus
-all = all.drop(columns=['id'])
-column_to_move = all.pop("src_subject_id") # moving id to first col
-data = all.copy() # to prevent fragmented dataframe
+df = data.drop(columns=['id'])
+column_to_move = df.pop("src_subject_id") # moving id to first col
+data = df.copy() # to prevent fragmented dataframe
 data.insert(0, "src_subject_id", column_to_move) # insert column with insert(location, column_name, column_value)
-
-df_bkup = data
-
 data = data.rename(columns={'src_subject_id':'IID', 'bpm_y_scr_internal_r':'bpm'})
 
-print('data is the current working df')
+# ================================ GENETIC VARS =====================================
 
-# ================================GENETIC VARS=====================================
-
-# change to var storage area
+# change to var storage area [check t2]
 os.chdir('/Users/poppygrimes/Library/CloudStorage/OneDrive-UniversityofEdinburgh/Edinburgh/prs/prs_bpm_OUT/t2')
 # all files containing PRSs [change from alspac to abcd]
 csvs = ['abcd_anx_prs_0320.best','abcd_neu_prs_0320.best',
         'abcd_mdd_prs_0320.best','abcd_scz_prs_0320.best',
         'abcd_asd_prs_0320.best','abcd_bip_prs_0320.best',
-        'abcd_adhd_prs_0320.best', 'abcd_meta_anx_prs_0405.best']
-
+        'abcd_adhd_prs_0320.best', 'abcd_meta_anx_prs_0405.best',
+        'mood_prs0501.best']
 
 # iterate over each file
 for csv_file in csvs:
@@ -136,17 +54,21 @@ for csv_file in csvs:
     col_prefix = csv_file.split('_')[1]
     # rename the second column to the appropriate prefix
     df = df.rename(columns={'PRS': col_prefix + '_prs'})
-    # merge the DataFrame with the 'als' DataFrame on the 'IID' column
+    # merge the DataFrame with the 'data' DataFrame on the 'IID' column
     data = pd.merge(data, df, on='IID', how='left')
 
 print('genetic data appended')
 
-# extract vars we want
-b_vars = ['SEX']
-c_vars = ['bpm', 'age']
+# sort the data by the 'IID' column
+data = data.sort_values('IID')
 
-# check time point for meta PRS output
-g_vars = ['scz_prs', 'neu_prs', 'mdd_prs','bip_prs', 'asd_prs', 'anx_prs', 'adhd_prs', 'meta_prs']
+# rename parcel cols
+data = data.rename(columns={'prs0501.best_prs':'mood_prs'})
+
+# extract vars we want
+b_vars = []
+c_vars = ['int_r', 'age']
+g_vars = ['scz_prs', 'neu_prs', 'mdd_prs','bip_prs', 'asd_prs', 'anx_prs', 'adhd_prs', 'meta_prs', 'mood_prs']
 dem_vars = ['IID','class','eventname']
 all_vars = dem_vars + b_vars + c_vars + g_vars
 
@@ -158,34 +80,25 @@ grouped = data.groupby(['eventname', 'class'])['class'].count().unstack(fill_val
 result = grouped.astype(int) # shows 0 NaNs at baseline measure
 print(result)
 
-# can idenitfy which class is which trajec
-bpm_table = pd.pivot_table(data, values='bpm', index='eventname', columns='class', aggfunc='mean')
+# table to identify which class is which trajectory
+bpm_table = pd.pivot_table(data, values='int_r', index='eventname', columns='class', aggfunc='mean')
 print(bpm_table)
 
-# select only vars we want
-data = data[all_vars]
-
-# binary vars restrict to [0,1,NaN]
-data[b_vars] = data[b_vars].mask(~data[b_vars].isin([0,1]))
-
-# standardise PRS
+data = data[all_vars] # select only vars we want
+data[b_vars] = data[b_vars].mask(~data[b_vars].isin([0,1])) # restrict binary vars to [0,1,NaN]
 for g_var in g_vars:
-    data[g_var] = preprocessing.scale(data[g_var])
+    data[g_var] = preprocessing.scale(data[g_var])  # standardise PRS
 
-
-# ============================= MN log reg =====================================
+# =============================== CLASSIFICATION =====================================
 
 # 'scz_prs', 'neu_prs', 'mdd_prs','bip_prs', 'asd_prs', 'anx_prs', 'adhd_prs'
 
-# Filter out rows with missing values in 'prs_mdd' and 'class' columns
-df2 = data.dropna(subset=['mdd_prs', 'class'])
+df2 = data.dropna(subset=['mood_prs', 'class']) # Filter out rows with missing values in vat and class cols
 df3 = df2.drop_duplicates(subset=["IID"])  # as data is in long format
+df3.loc[:, 'class'] = df3['class'].replace(1.0, 0.0)  # change reference class (non-depressed) to 0
 
-# class 3 is stable low for bpm > change to 0 for reference
-# c1 high, c3 decreasing, c4 increasing
-
-df3.loc[:, 'class'] = df3['class'].replace(3.0, 0.0)  # replace with 0 for ref level
-x = df3['mdd_prs']
+# fit mn logistic regression model
+x = df3['mood_prs']
 y = df3['class']
 X = sm.add_constant(x)
 model = sm.MNLogit(y, X)
@@ -199,7 +112,7 @@ print(conf_int_exp)
 
 
 ####################### linear regression of MDD PRS and BPM at 6 time points
-df = data
+'''df = data
 df = df.dropna(subset=['mdd_prs', 'bpm'])
 event_names = df['eventname'].unique()
 regression_results = {}
@@ -216,4 +129,4 @@ for event_name in event_names:
 
     print(f'Regression results for eventname: {event_name}')
     print(results.summary())
-    print('---------------------------------------------------------------------')
+    print('---------------------------------------------------------------------')'''
--- alspac_prs_predict.py
+++ alspac_prs_predict.py
@@ -1,59 +1,37 @@
-# gmm predict script but for genetic derived vars only
-# just need: PRS, id, og_id, class data
-
 import numpy as np
 import os
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 import pyreadr
-import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegression, LinearRegression
-from sklearn import tree
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-import scipy.stats as stats
 import statsmodels.api as sm
-from pathlib import Path
 from sklearn import preprocessing
 
+# ================================ APPEND CLASS ENUMERATION =================================
 
-# reading in smfq data, previously cleaned and scores calculated in R script
-df = pyreadr.read_r('/Users/poppygrimes/Library/CloudStorage/OneDrive-UniversityofEdinburgh/Edinburgh/gmm/gmm_alspac/alspac_dep_long.rds')
-print(df.keys()) #check which objects we have: there is only None
-df = df[None]
-df['id'].nunique() #check 15,645
-
-# convert to wide - remember to only use first 4 time points in mplus
-df = df[['id','time','dep']]
-df_wide = pd.pivot(df, index=['id'], columns='time', values='dep') #should have 15,645 rows
-df_wide = df_wide.fillna('-9999') #replace NaNs with -9999 for mplus
-filepath = Path('/Users/poppygrimes/Library/CloudStorage/OneDrive-UniversityofEdinburgh/Edinburgh/gmm/gmm_alspac/mplus_data/alspac_smfq_wide_python.txt')
-filepath.parent.mkdir(parents=True, exist_ok=True)
-df_wide.to_csv(filepath, header=False, sep='\t')  #save wide data for mplus in gmm_abcd directory
-
-# =================================================
-# make new df with id, unique id, Xvars and yclass
-alspac_4k = pd.read_table('/Users/poppygrimes/Library/CloudStorage/OneDrive-UniversityofEdinburgh/Edinburgh/gmm/gmm_alspac/mplus_data/4k_alspac_smfq.txt', delim_whitespace=True, header=None)  # this is just test will need to change
+# class data from mplus
+alspac_4k = pd.read_table('/Users/poppygrimes/Library/CloudStorage/OneDrive-UniversityofEdinburgh/Edinburgh/gmm/gmm_alspac/mplus_data/4k_gmm_smfq.txt', delim_whitespace=True, header=None)  # this is just test will need to change
 alspac_4k.columns = ['y0','y1','y2','y3','v1','v2','v3','v4','v5','v6','v7','v8','v9','v10','class','id']
 alspac_4k = alspac_4k[['id','class']] #subsetting
 alspac_4k.replace('*', np.nan) #replace missing
-# merge class with smfq data -
-data = pd.merge(df, alspac_4k, on=["id"])
-# n8787 unique ids x 11 time points and classes - exclude after 4 time points as this is what model was built on
-data = data[data["time"] < 5]
 
-# ============================ EXTRACTING VARS ===================================
+# smfq data - scores calculated in R (alspac_smfq_scores.Rmd)
+df = pyreadr.read_r('/Users/poppygrimes/Library/CloudStorage/OneDrive-UniversityofEdinburgh/Edinburgh/gmm/gmm_alspac/alspac_dep_long.rds')
+df = df[None]
+df['id'].nunique() #check 15,645
+df = df[['id','time','dep']]
 
+data = pd.merge(df, alspac_4k, on=["id"]) # merge class with smfq data
+data = data[data["time"] < 5] # exclude after 4 time points as this is what model was built on
+
+print('class data merged')
+
+# =================================== EXTRACTING VARS ===================================
+
+# takes a while to read in
 alspac = pd.read_stata("/Volumes/cmvm/scs/groups/ALSPAC/data/B3421_Whalley_04Nov2021.dta")
-als = alspac # call again if need clean
 
 # select only cols we want
 als_cols = ['cidB3421','qlet','kz021','c804']
-als = als[als_cols]
+als = alspac[als_cols]
 
 # make alspac have same ids as originally coded in R
 als['cidB3421'] = als['cidB3421'].astype(int)
@@ -65,16 +43,14 @@ als = als.rename(columns={'kz021':'sex', 'c804':'ethnicity'}) # rename some cols
 column_to_move = als.pop("id") # moving id to first col
 als.insert(0, "id", column_to_move) # insert column with insert(location, column_name, column_value)
 
-als2 = als # back up
-
-# ============================ appending PRS scores ===================================
+print('alspac data read in as als')
+# ============================ GENETIC SCORES ===================================
 
 # change to prs.best storage area
 os.chdir('/Users/poppygrimes/Library/CloudStorage/OneDrive-UniversityofEdinburgh/Edinburgh/prs/prs_alspac_OUT')
-# all files containing PRSs
 csvs = ['alspac_scz_prs_0317.best','alspac_neu_prs_0316.best','alspac_mdd_prs_0320.best','alspac_bip_prs_0317.best',
        'alspac_asd_prs_0317.best','alspac_anx_prs_0316.best', 'alspac_adhd_prs_0317.best', 'alspac_meta_anx_prs_0405.best',
-        'alspac_cf_prs_0418.best']
+        'alspac_cf_prs_0418.best', 'mood_prs0501.best']
 
 # iterate over each file
 for csv_file in csvs:
@@ -89,45 +65,40 @@ for csv_file in csvs:
 
 print(als.head())
 
-# ==============================================================================================
-# ================================= appending class data =======================================
+als = als.rename(columns={'prs0501.best_prs':'mood_prs'})
 
-# make ids same object type - both integers
-# alspac_4k is a df of ids and classes (8787 x 2)
+# ==============================================================================================
+
 # merge als with alspac_4k by id [but need to make sure these IDs are the same
-alspac_4k['id'] = alspac_4k['id'].astype(int) # only id and class
+alspac_4k['id'] = alspac_4k['id'].astype(int) # make ids same object type (int)
 df = pd.merge(alspac_4k, als, on=["id"])
 
 # select and clean variables
 dem_vars = ['id', 'IID', 'ethnicity', 'class'] # demographic
-g_vars = ['scz_prs', 'neu_prs', 'mdd_prs','bip_prs', 'asd_prs', 'anx_prs', 'adhd_prs', 'meta_prs','cf_prs'] # genetic
+g_vars = ['scz_prs', 'neu_prs', 'mdd_prs','bip_prs', 'asd_prs', 'anx_prs', 'adhd_prs', 'meta_prs','cf_prs', 'mood_prs'] # genetic
 b_vars = ['sex'] # binary
 all_vars = dem_vars + g_vars + b_vars
 X_vars = g_vars + b_vars
 
-# ==========================================================================================
 # ================================= MAKING DESIGN MATRIX ===================================
 
 df = df[all_vars] # select only vars we want
-
-# binary vars restrict to [0,1,NaN]
 df['sex'] = df['sex'].replace(['Female','Male'], [1,0]) # first recode sex
-df[b_vars] = df[b_vars].mask(~df[b_vars].isin([0,1]))
+df[b_vars] = df[b_vars].mask(~df[b_vars].isin([0,1])) # binary vars restrict to [0,1,NaN]
 
-# standardise PRS
 for g_var in g_vars:
-    df[g_var]  = preprocessing.scale(df[g_var])
+    df[g_var]  = preprocessing.scale(df[g_var])  # standardise PRS
 
 # ============================= MN log reg =====================================
 
 # 'scz_prs', 'neu_prs', 'mdd_prs','bip_prs', 'asd_prs', 'anx_prs', 'adhd_prs'
 
-# Filter out rows with missing values in 'prs_mdd' and 'class' columns
-df2 = df.dropna(subset=['cf_prs', 'class'])
+df2 = df.dropna(subset=['neu_prs', 'class']) # Filter out rows with missing vals in prs and class
 df3 = df2.drop_duplicates(subset=["id"]) # as data is in long format
+df3.loc[:, 'class'] = df3['class'].replace(2.0, 0.0) # replace with 0 for ref level
 
-df3.loc[:, 'class'] = df3['class'].replace(3.0, 0.0) # replace with 0 for ref level
-x = df3['cf_prs']
+# fit model
+x = df3['neu_prs']
 y = df3['class']
 X = sm.add_constant(x)
 model = sm.MNLogit(y, X)
